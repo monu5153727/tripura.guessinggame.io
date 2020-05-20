bd03afda9653bd196c3bbb5b4555941d1e8dba8b
monu5153727 <40862497+monu5153727@users.noreply.github.com>
2020-05-20 16:22:52 +0530

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,61 +1,51 @@
 #!/bin/bash
 
- echo -n "guess how  many files in this directory"
+ 
 
-read num
+
 
 #function 
 function Guess
 
 {
-  check=0
-  
-while !  [[ "$num" =~ ^[0-9]+$ ]] ; 
- do
+  filepresent=$(ls -1a | wc -l)
+
+  #loop
+  while true 
+  do
+
+  echo -n "guess how  many files in this directory"
+
+read num
+
+#if condition
+if !  [[ "$num" =~ ^[0-9]+$ ]] ; 
+ then
  echo 'please enter valid input'
- read num
-let check=1
+
+
+elif [[ $num -lt $filepresent ]]
+ then
+echo 'oops! guess is lower'
+
+elif [[ $num -gt $filepresent ]]
+ then
+echo 'oops! guess is high'
+
+
+else 
+
+echo 'guess is right'
+break
+
+fi
+
+
+
 done
 }
 
 
-
-filepresent=$(ls -1a | wc -l)
-  
-
-
-
-
 Guess
 
-
-
-
-
-#loop
- while [[ $num -ne $filepresent ]]
- do
-
-  #if condition
- if [[ $num -lt $filepresent ]]
- then
-echo 'oops! guess is lower'
-read num
-Guess
- #if it guess is high
-else 
-echo 'oops! guess is higher'
-read num
-Guess
-fi
-
-echo 'correct guess'
-
-read num
-Guess
-done
-
-if [[ $num -eq $filepresent ]]
-then
-echo 'congrat Guess  is actually correct'
-fi
+ 
